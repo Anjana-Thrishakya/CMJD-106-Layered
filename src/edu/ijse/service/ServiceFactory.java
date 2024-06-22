333eaf7c38a0e14e10b46d36a2ac025b649009c2
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-06-22 13:46:45 +0530

    Super Service and Service Factory

---
+++ src/edu/ijse/service/ServiceFactory.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.service;
+
+/**
+ *
+ * @author anjan
+ */
+public class ServiceFactory {
+    private static ServiceFactory serviceFactory;
+    
+    private ServiceFactory(){}
+    
+    public static ServiceFactory getInstance(){
+        if(serviceFactory == null){
+            serviceFactory = new ServiceFactory();
+        }
+        return serviceFactory;
+    }
+    
+    public SuperService getService(ServiceType serviceType){
+        switch (serviceType) {
+            case ITEM:
+                return null;
+            default:
+                return null;
+        }
+    }
+    
+    public enum ServiceType{
+        ITEM
+    }
+}
