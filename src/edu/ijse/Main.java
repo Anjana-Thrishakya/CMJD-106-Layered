8ae123a97c57e06af24979664857e7816177da16
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-06-29 10:19:16 +0530

    ITEM CRUD Complete

--- src/edu/ijse/Main.java
+++ src/edu/ijse/Main.java
@@ -4,6 +4,8 @@
  */
 package edu.ijse;
 
+import edu.ijse.view.ItemView;
+
 /**
  *
  * @author anjan
@@ -13,8 +15,8 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws Exception {
+        new ItemView().setVisible(true);
     }
     
 }
